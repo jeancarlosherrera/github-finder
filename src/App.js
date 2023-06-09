b801ef5a3b2956cd751505d32b3ad93f2f63e5e0
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-06-09 19:58:34 -0400

    Boilerplate and routes added for pages components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/notfound' element={<NotFound />}></Route>
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
