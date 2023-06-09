3022c99b4b2ac637298d34fa355613d7d77519bc
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-06-09 19:32:10 -0400

    Footer component created

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  const footerYear = new Date().getFullYear()
+  return (
+    <footer className='footer p-10 bg-gray-700 footer-center'>
+      <div>
+        <p>Copyright &copy;{footerYear} Github Finder</p>
+        <p>All rights reserved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
